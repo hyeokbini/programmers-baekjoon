25441af5d93171c6c017fa73adb256c220192224
고혁빈 <137269496+hyeokbini@users.noreply.github.com>
2024-04-03 14:02:42 +0900

    [level 1] Title: 성격 유형 검사하기, Time: 0.10 ms, Memory: 4.14 MB -BaekjoonHub

---
+++ 프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.cpp
@@ -0,0 +1,97 @@
+#include <string>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+int chooseidx (string survey)
+{
+    if(survey[0] == 'R' || survey[0] == 'T')
+    {
+        return 0;
+    }
+    else if(survey[0] == 'F' || survey[0] == 'C')
+    {
+        return 1;
+    }
+    else if(survey[0] == 'M' || survey[0] == 'J')
+    {
+        return 2;
+    }
+    else
+    {
+        return 3;
+    }
+}
+string solution(vector<string> survey, vector<int> choices) {
+    string answer = "";
+    vector<int> num (4,0); // 양수가 RCJA
+    int idx = 0;
+    for(int i = 0; i < survey.size(); i++)
+    {
+        idx = chooseidx(survey[i]);
+        if(choices[i] == 4)
+        {
+            continue;
+        }
+        else if(choices[i] < 4) // 비동의 - RCJA는 음수값, 나머지는 양수값 더해져야함
+        {
+            if(survey[i][0] == 'R' || survey[i][0] == 'C' || survey[i][0] == 'J' || survey[i][0] == 'A')
+            {
+                num[idx] -= choices[i] - 4;
+            }
+            else
+            {
+                num[idx] += choices[i] - 4;
+            }
+        }
+        else // 동의 - RCJA에 양수값 더해져야함
+        {
+            if(survey[i][0] == 'R' || survey[i][0] == 'C' || survey[i][0] == 'J' || survey[i][0] == 'A')
+            {
+                num[idx] -= choices[i] - 4;
+            }
+            else
+            {
+                num[idx] += choices[i] - 4;
+            }
+        }
+    }
+    for(auto i : num)
+    {
+        cout << i << ' ';
+    }
+    if(num[0] >= 0)
+    {
+        answer += 'R';
+    }
+    else
+    {
+        answer += 'T';
+    }
+    if(num[1] >= 0)
+    {
+        answer += 'C';
+    }
+    else
+    {
+        answer += 'F';
+    }
+    if(num[2] >= 0)
+    {
+        answer += 'J';
+    }
+    else
+    {
+        answer += 'M';
+    }
+    if(num[3] >= 0)
+    {
+        answer += 'A';
+    }
+    else
+    {
+        answer += 'N';
+    }
+    return answer;
+}
