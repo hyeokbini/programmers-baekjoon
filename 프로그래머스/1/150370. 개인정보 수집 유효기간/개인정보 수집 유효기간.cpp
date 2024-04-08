464761831cecfabe69599ef73d3ea950c459563c
고혁빈 <137269496+hyeokbini@users.noreply.github.com>
2024-04-08 15:24:26 +0900

    [level 1] Title: 개인정보 수집 유효기간, Time: 0.05 ms, Memory: 4.09 MB -BaekjoonHub

--- 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.cpp
+++ 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.cpp
@@ -10,7 +10,9 @@ vector<int> solution(string today, vector<string> terms, vector<string> privacie
     int compb = 0;
     for(int i = 0; i < privacies.size(); i++)
     {
-        compa = (inttoday[0] - stoi(privacies[i].substr(0,4))) * 336 + (inttoday[1] - stoi(privacies[i].substr(5,2))) * 28 + (inttoday[2] - stoi(privacies[i].substr(8,2)));
+        compa = (inttoday[0] - stoi(privacies[i].substr(0,4))) * 336 + 
+            (inttoday[1] - stoi(privacies[i].substr(5,2))) * 28 + 
+            (inttoday[2] - stoi(privacies[i].substr(8,2)));
         for(int j = 0; j < terms.size(); j++)
         {
             if(terms[j][0] == privacies[i][11])
