140f1e9e81b080c518c774cecded09ad27cd0196
고혁빈 <137269496+hyeokbini@users.noreply.github.com>
2024-04-15 17:55:10 +0900

    Update 유기농 배추.cc

--- 백준/Silver/1012. 유기농 배추/유기농 배추.cc
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.cc
@@ -15,28 +15,28 @@ int main()
     int T;
     cin >> T;
 
-    while (T--) // 테스트 케이스의 개수만큼 반복
+    while (T--)
     {
         int M, N, K;
         cin >> M >> N >> K;
         
-        vector<vector<int>> map(N, vector<int>(M, 0)); // M과 N을 반대로 사용하여 초기화
-        vector<vector<bool>> vis(N, vector<bool>(M, false)); // M과 N을 반대로 사용하여 초기화
+        vector<vector<int>> map(N, vector<int>(M, 0));
+        vector<vector<bool>> vis(N, vector<bool>(M, false));
 
         for (int i = 0; i < K; i++)
         {
             int x, y;
             cin >> x >> y;
-            map[y][x] = 1; // (y, x)로 배열에 값을 설정
+            map[y][x] = 1;
         }
 
         int answer = 0;
 
-        for (int i = 0; i < N; i++) // N으로 바꿔서 변경
+        for (int i = 0; i < N; i++)
         {
-            for (int j = 0; j < M; j++) // M으로 바꿔서 변경
+            for (int j = 0; j < M; j++) 
             {
-                if (map[i][j] == 0 || vis[i][j] == true) // 인덱스 접근 부분 수정
+                if (map[i][j] == 0 || vis[i][j] == true) 
                     continue;
 
                 answer++;
@@ -55,7 +55,7 @@ int main()
                         int nx = cur.first + dx[k];
                         int ny = cur.second + dy[k];
 
-                        if (nx < 0 || ny < 0 || nx >= N || ny >= M) // 인덱스 범위 검사 수정
+                        if (nx < 0 || ny < 0 || nx >= N || ny >= M) 
                             continue;
 
                         if (map[nx][ny] == 0 || vis[nx][ny] == true)
