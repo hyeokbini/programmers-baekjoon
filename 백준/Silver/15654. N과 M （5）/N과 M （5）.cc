10c75209f6139aa7bfcd8c27f1ac91c1167c6be2
고혁빈 <137269496+hyeokbini@users.noreply.github.com>
2024-05-24 18:09:52 +0900

    [Silver III] Title: N과 M (5), Time: 40 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.cc
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int n,m;
+vector<int> arr;
+int ans[10];
+bool visited[10];
+
+void func(int cur)
+{
+    if(cur == m)
+    {
+        for(int i = 0; i < m; i++)
+        {
+            cout << ans[i] << " ";
+        }
+        cout << "\n";
+        return;
+    }
+    for(int i = 0; i < n; i++)
+    {
+        if(!visited[i])
+        {
+            ans[cur] = arr[i];
+            visited[i] = 1;
+            func(cur + 1);
+            visited[i] = 0;
+        }
+    }
+}
+
+int main()
+{
+    cin >> n >> m;
+    int tmp;
+    for(int i = 0; i < n; i++)
+    {
+        cin >> tmp;
+        arr.push_back(tmp);
+    }
+    sort(arr.begin(),arr.end());
+    func(0);
+}
