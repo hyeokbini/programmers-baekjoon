5aed3c1860dcf78916271764a403fb8bcc897844
고혁빈 <137269496+hyeokbini@users.noreply.github.com>
2024-06-11 19:49:43 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 192 ms, Memory: 7540 KB -BaekjoonHub

---
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.cc
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+bool comp(vector<int> arr1, vector<int> arr2)
+{
+    if (arr1[0] < arr2[0])
+    {
+        return arr1[0] < arr2[0];
+    }
+    else if (arr1[0] == arr2[0])
+    {
+        return arr1[1] < arr2[1];
+    }
+    else
+    {
+        return false;
+    }
+}
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    //freopen("test.txt","rt",stdin);
+    int n;
+    cin >> n;
+    vector<vector<int>> arr(n, vector<int>(2));
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i][0] >> arr[i][1];
+    }
+    sort(arr.begin(), arr.end(), comp);
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i][0] << " " << arr[i][1] << "\n";
+    }
+    return 0;
+}
