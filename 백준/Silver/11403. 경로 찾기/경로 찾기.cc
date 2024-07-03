88fd491e7fe5c9837befa4a1c78a6ddce18df7fd
고혁빈 <137269496+hyeokbini@users.noreply.github.com>
2024-07-04 03:07:38 +0900

    [Silver I] Title: 경로 찾기, Time: 4 ms, Memory: 2152 KB -BaekjoonHub

---
+++ 백준/Silver/11403. 경로 찾기/경로 찾기.cc
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    //freopen("test.txt", "rt", stdin);
+    int n;
+    cin >> n;
+    vector<vector<int>> arr(n, vector<int>(n));
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            cin >> arr[i][j];
+        }
+    }
+
+    for (int k = 0; k < n; k++)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                if (arr[i][k] + arr[k][j] == 2)
+                {
+                    arr[i][j] = 1;
+                }
+            }
+        }
+    }
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            cout << arr[i][j] << " ";
+        }
+        cout << "\n";
+    }
+    return 0;
+}
