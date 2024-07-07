75f6279855945345a71c0c16b2fb217bb4dbc513
고혁빈 <137269496+hyeokbini@users.noreply.github.com>
2024-07-08 03:54:12 +0900

    [Bronze II] Title: 단어의 개수, Time: 8 ms, Memory: 3680 KB -BaekjoonHub

---
+++ 백준/Bronze/1152. 단어의 개수/단어의 개수.cc
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    //freopen("test.txt", "rt", stdin);
+    string tmp;
+    int n = 0;
+    while(cin >> tmp)
+    {
+        n++;
+    }
+    cout << n;
+    return 0;
+}
