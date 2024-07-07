7fb02b0dae641d123c4a7b595a30edf97910a79a
고혁빈 <137269496+hyeokbini@users.noreply.github.com>
2024-07-08 03:55:22 +0900

    [Bronze V] Title: N 찍기, Time: 8 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2741. N 찍기/N 찍기.cc
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    //freopen("test.txt", "rt", stdin);
+    int n;
+    cin >> n;
+    for(int i = 1; i <= n; i++)
+    {
+        cout << i << "\n";
+    }
+    return 0;
+}
