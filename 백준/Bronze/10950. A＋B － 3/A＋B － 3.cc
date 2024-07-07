94aa50ee9bb9ec83bebfbd2acc144f798df799d9
고혁빈 <137269496+hyeokbini@users.noreply.github.com>
2024-07-08 03:32:57 +0900

    [Bronze V] Title: A+B - 3, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.cc
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    //freopen("test.txt", "rt", stdin);
+    int n;
+    cin >> n;
+    while(n--)
+    {
+        int a,b;
+        cin >> a >> b;
+        cout << a + b << "\n";
+    }
+    return 0;
+}
