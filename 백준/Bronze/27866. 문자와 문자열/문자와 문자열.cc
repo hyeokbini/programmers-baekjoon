14b43cbba1293b6a49fb8905b7e2f37e7a7db35c
고혁빈 <137269496+hyeokbini@users.noreply.github.com>
2024-07-08 03:52:05 +0900

    [Bronze V] Title: 문자와 문자열, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/27866. 문자와 문자열/문자와 문자열.cc
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    // freopen("test.txt", "rt", stdin);
+    string tmp;
+    int n;
+    cin >> tmp >> n;
+    cout << tmp[n - 1];
+    return 0;
+}
