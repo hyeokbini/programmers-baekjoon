9df1a753cca8487ade7e99e1312cc0295e4d915a
고혁빈 <137269496+hyeokbini@users.noreply.github.com>
2024-07-08 03:51:06 +0900

    [Bronze V] Title: 새싹, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.cc
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    // freopen("test.txt", "rt", stdin);
+    cout << "         ,r\'\"7" << endl;
+    cout << "r`-_   ,'  ,/" << endl;
+    cout << " \\. \". L_r'" << endl;
+    cout << "   `~\\/" << endl;
+    cout << "      |" << endl;
+    cout << "      |" << endl;
+    return 0;
+}
