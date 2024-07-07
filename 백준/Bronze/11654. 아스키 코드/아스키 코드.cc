f3311fc74396d3881be4137a668b1b3b683470c1
고혁빈 <137269496+hyeokbini@users.noreply.github.com>
2024-07-08 03:40:48 +0900

    [Bronze V] Title: 아스키 코드, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.cc
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    // freopen("test.txt", "rt", stdin);
+    char a;
+    cin >> a;
+    cout << (int)a;
+    return 0;
+}
