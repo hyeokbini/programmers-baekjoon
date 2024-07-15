69d6c17411f2927e381b6a2893f0372e075acd66
고혁빈 <137269496+hyeokbini@users.noreply.github.com>
2024-07-16 05:54:59 +0900

    [Bronze V] Title: 코딩은 체육과목 입니다, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/25314. 코딩은 체육과목 입니다/코딩은 체육과목 입니다.cc
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    //freopen("test.txt", "rt", stdin);
+    int n;
+    cin >> n;
+    n /= 4;
+    while(n--)
+    {
+        cout << "long ";
+    }
+    cout << "int";
+    return 0;
+}
