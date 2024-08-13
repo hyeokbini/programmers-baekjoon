eb4470102972004317f197991086d3566498d3b9
고혁빈 <137269496+hyeokbini@users.noreply.github.com>
2024-08-14 02:57:33 +0900

    [Bronze V] Title: 초코바, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/27959. 초코바/초코바.cc
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    //freopen("test.txt", "rt", stdin);
+    int n,m;
+    cin >> n >> m;
+    (n*100 >= m) ? cout << "Yes" : cout << "No";
+    return 0;
+}
