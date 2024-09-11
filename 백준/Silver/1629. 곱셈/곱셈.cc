1b1a277178221c408622aded643f526b71943b14
고혁빈 <137269496+hyeokbini@users.noreply.github.com>
2024-09-11 10:25:13 +0900

    [Silver I] Title: 곱셈, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1629. 곱셈/곱셈.cc
+++ 백준/Silver/1629. 곱셈/곱셈.cc
@@ -2,34 +2,25 @@
 
 using namespace std;
 
-long long int a, b, c;
-unordered_map<int, long long int> dp;
+long long int a,b,c;
 
 long long int func(int val)
 {
-    if (dp.find(val) != dp.end())
+    if(val == 1)
     {
-        return dp[val];
-    }
-
-    if (val == 1)
-    {
-        return dp[val] = a % c;
+        return a % c;
     }
 
     long long int tmpval = func(val / 2);
-    long long int result;
 
-    if (val % 2 == 0)
+    if(val % 2 == 0)
     {
-        result = tmpval * tmpval % c;
+        return tmpval * tmpval % c;
     }
     else
     {
-        result = tmpval * tmpval % c * a % c;
+        return tmpval * tmpval % c * a % c;
     }
-
-    return dp[val] = result;
 }
 
 int main()
@@ -37,10 +28,7 @@ int main()
     ios::sync_with_stdio(false);
     cin.tie(0);
     //freopen("test.txt", "rt", stdin);
-    
     cin >> a >> b >> c;
-
-    cout << func(b) << endl;
-
+    cout << func(b);
     return 0;
 }
