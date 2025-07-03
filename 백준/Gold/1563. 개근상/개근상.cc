fd60f3a2252cef49e0543d20066e5c7c28aea90d
고혁빈 <137269496+hyeokbini@users.noreply.github.com>
2025-07-04 03:01:49 +0900

    [Gold IV] Title: 개근상, Time: 0 ms, Memory: 2044 KB -BaekjoonHub

--- 백준/Gold/1563. 개근상/개근상.cc
+++ 백준/Gold/1563. 개근상/개근상.cc
@@ -10,12 +10,13 @@ int main()
     cin.tie(0);
     //freopen("test.txt", "rt", stdin);
     int n;
+    int ans = 0;
     cin >> n;
     dp[1][0][0] = 1; // O
     dp[1][1][0] = 1; // L
     dp[1][0][1] = 1; // A
 
-    for (int i = 2; i <= n; ++i)
+    for (int i = 2; i <= n; i++)
     {
         // 전날 L별 총합
         int latezero = (dp[i - 1][0][0] + dp[i - 1][0][1] + dp[i - 1][0][2]) % m;
@@ -32,13 +33,11 @@ int main()
         dp[i][0][2] = dp[i - 1][0][1] % m;
         dp[i][1][2] = dp[i - 1][1][1] % m;
     }
-
-    int ans = 0;
-    for (int l = 0; l <= 1; ++l)
+    for (int i = 0; i <= 1; i++)
     {
-        for (int a = 0; a <= 2; ++a)
+        for (int j = 0; j <= 2; j++)
         {
-            ans = (ans + dp[n][l][a]) % m;
+            ans = (ans + dp[n][i][j]) % m;
         }
     }
 
